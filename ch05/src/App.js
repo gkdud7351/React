202d53b84a337d3de6a6ef8da37d08f57f31b341
gkdud7351 <gkdud7351@gmail.com>
2024-10-17 16:21:57 +0900

    'ch05'

--- ch05/src/App.js
+++ ch05/src/App.js
@@ -1,19 +1,21 @@
-import logo from "./logo.svg"
-import "./App.css"
-import IterationSample from "./IterationSample"
-import IterationSample2 from "./IterationSample2"
-import IterationSample3 from "./IterationSample3"
-import IterationSample4 from "./IterationSample4"
-import IterationSample5 from "./IterationSample5"
-import IterationSample6 from "./IterationSample6"
+//import logo from "./logo.svg";
+import "./App.css";
+//import IterationSample from "./IterationSample";
+//import IterationSample2 from "./IterationSample2";
+//import IterationSample3 from "./IterationSample3";
+//mport IterationSample4 from "./IterationSample4";
+import IterationSample5 from "./IterationSample5";
+///import IterationSample5_self from "./IterationSample5_self";
+//import IterationSample6 from "./IterationSample6";
 
 function App() {
-   //return <IterationSample />
-   //return <IterationSample2 />
-   //return <IterationSample3 />
-   //return <IterationSample4 />
-   //return <IterationSample5 />
-   return <IterationSample6 />
+  //return <IterationSample />;
+  //return <IterationSample2 />;
+  //return <IterationSample3 />
+  //return <IterationSample4 />;
+  return <IterationSample5 />;
+  //return <IterationSample5_self />;
+  //<IterationSample6 />;
 }
 
-export default App
+export default App;
--- ch05/src/IterationSample.js
+++ ch05/src/IterationSample.js
@@ -1,7 +1,9 @@
-function IterationSample() {
-   const names = ["눈사람", "얼음", "눈", "바람"]
-   const nameList = names.map((name) => <li>{name}</li>)
-   return <ul>{nameList}</ul>
-}
+import React from "react";
 
-export default IterationSample
+const IterationSample = () => {
+  const names = ["눈사람", "얼음", "눈", "바람"];
+  const nameList = names.map((name) => <li>{name}</li>);
+  return <ul>{nameList}</ul>;
+};
+
+export default IterationSample;
